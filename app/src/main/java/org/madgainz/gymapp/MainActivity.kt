a23441f5109c2f9ed92740e1cd7cfc0345464007
Luca Violanti <lv185028@ncr.com>
2018-08-01 16:31:53 +0100

    Temp stuff

--- app/src/main/java/org/madgainz/gymapp/MainActivity.kt
+++ app/src/main/java/org/madgainz/gymapp/MainActivity.kt
@@ -9,26 +9,54 @@ import android.view.MenuItem
 import android.widget.Chronometer
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import org.madgainz.gymapp.MainActivity.State.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var state = STOPPED
+    private lateinit var mp: MediaPlayer
+
+    private enum class State(val time: Long) {
+        STOPPED(0), WARM_UP(10000), COOL_DOWN(15000), EXERCISE(45000)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        val mp: MediaPlayer = MediaPlayer.create(this, R.raw.beep)
+        mp = MediaPlayer.create(this, R.raw.beep)
 
-        chronometer.base = SystemClock.elapsedRealtime() + 2000
         chronometer.onChronometerTickListener = Chronometer.OnChronometerTickListener { chronometer ->
-            if ("-00:02" == chronometer.text) {
-                mp.start()
+            if (isTimedOut()) {
+               state = when (state) {
+                   MainActivity.State.STOPPED -> STOPPED
+                   WARM_UP -> switchStateTo(EXERCISE, chronometer)
+                   EXERCISE -> switchStateTo(COOL_DOWN, chronometer)
+                   COOL_DOWN -> switchStateTo(EXERCISE, chronometer)
+               }
             }
         }
         beepFab.setOnClickListener {
-            mp.start()
-            chronometer.start()
+            if (state == STOPPED) {
+                switchStateTo(WARM_UP, chronometer)
+            } else {
+                state = STOPPED
+                chronometer.stop()
+            }
         }
     }
+
+    private fun switchStateTo(newState: State, chronometer: Chronometer): State {
+        mp.start()
+        chronometer.stop()
+        chronometer.base = SystemClock.elapsedRealtime() + getTimeRemaining(newState)
+        chronometer.start()
+        return newState
+    }
+
+    private fun getTimeRemaining(state: State) = SystemClock.elapsedRealtime() + state.time
+
+    private fun isTimedOut() = SystemClock.elapsedRealtime() == chronometer.base
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
